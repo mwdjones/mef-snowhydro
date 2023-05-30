4db581ae61e8fb5b07ba3432b5e06a3a4529da26
M W Jones <jone3247@umn.edu>
2023-05-30 12:20:41 -0500

    begun time series analysis

--- Data and Codes/02_snowdataanalysis.py
+++ Data and Codes/02_snowdataanalysis.py
@@ -17,7 +17,7 @@ import numpy as np
 import seaborn as sns
 import scipy
 import xarray as xr
-import statsmodels
+import statsmodels.api as sm
 
 #%%
 '''IMPORT DATA'''
@@ -31,6 +31,8 @@ s2data_df = s2data_df.replace('NaN', np.nan)
 s2data_df = s2data_df.replace('nan', np.nan)
 s2data_df.time = pd.to_datetime(s2data_df.time)
 s2data_df['watershed'] = 'S2'
+#Save separately
+s2data_df.to_csv(import_path + '01_cleanedsnowdataS2.csv')
 
 s6data = xr.open_dataset(import_path + '01_cleanedsnowdataS6.nc')
 s6data_df = s6data.to_dataframe().reset_index(drop = False)
@@ -38,6 +40,7 @@ s6data_df = s6data_df.replace('NaN', np.nan)
 s6data_df = s6data_df.replace('nan', np.nan)
 s6data_df.time = pd.to_datetime(s6data_df.time)
 s6data_df['watershed'] = 'S6'
+s6data_df.to_csv(import_path + '01_cleanedsnowdataS6.csv')
 
 
 ### Import Frost Data
@@ -135,13 +138,55 @@ s6Bog = s6_grouped[s6_grouped.zones == 'Bog']
 s6Lagg = s6_grouped[s6_grouped.zones == 'Lagg']
 s6Upland = s6_grouped[s6_grouped.zones == 'Upland']
 
+## Autocorrelationfunctions
 #Is the variance, mean and autocovariance of the snow depth relatively stable over time?
-from statsmodels.graphics.tsaplots import plot_acf
+from statsmodels.graphics.tsaplots import plot_acf, plot_pacf
 
-plot_acf(np.array(s6Bog.depths.squeeze()))
-plot_acf(np.array(s6Lagg.depths.squeeze()))
-plot_acf(np.array(s6Upland.depths.squeeze()))
+#S6
+fig, ax = plt.subplots(3, 2, figsize = (12, 9))  
 
-#
+s6bog_acf = plot_acf(np.array(s6Bog.depths.squeeze()), ax = ax[0][0])
+s6lagg_acf = plot_acf(np.array(s6Lagg.depths.squeeze()), ax = ax[1][0], title = ' ')
+s6upland_acf = plot_acf(np.array(s6Upland.depths.squeeze()), ax = ax[2][0], title = ' ')
+s6bog_pacf = plot_pacf(np.array(s6Bog.depths.squeeze()), lags = 6, ax = ax[0][1])
+s6lagg_pacf = plot_pacf(np.array(s6Lagg.depths.squeeze()), lags = 6, ax = ax[1][1], title = ' ')
+s6upland_pacf = plot_pacf(np.array(s6Upland.depths.squeeze()), lags = 6, ax = ax[2][1], title = ' ')
+
+plt.suptitle('Autocorrelation functions in S6')
+ax[0][0].set_xlabel('Bog')
+ax[1][0].set_xlabel('Lagg')
+ax[2][0].set_xlabel('Upland')
+plt.show()
+
+#S2
+fig, ax = plt.subplots(3, 2, figsize = (12, 9))  
+
+s2bog_acf = plot_acf(np.array(s2Bog.depths.squeeze()), ax = ax[0][0])
+s2lagg_acf = plot_acf(np.array(s2Lagg.depths.squeeze()), ax = ax[1][0], title = ' ')
+s2upland_acf = plot_acf(np.array(s2Upland.depths.squeeze()), ax = ax[2][0], title = ' ')
+s2bog_pacf = plot_pacf(np.array(s2Bog.depths.squeeze()), lags = 6, ax = ax[0][1])
+s2lagg_pacf = plot_pacf(np.array(s6Lagg.depths.squeeze()), lags = 6, ax = ax[1][1], title = ' ')
+s2upland_pacf = plot_pacf(np.array(s2Upland.depths.squeeze()), lags = 6, ax = ax[2][1], title = ' ')
+
+plt.suptitle('Autocorrelation functions in S2')
+ax[0][0].set_xlabel('Bog')
+ax[1][0].set_xlabel('Lagg')
+ax[2][0].set_xlabel('Upland')
+plt.show()
+
+# %%
+
+### Snow vs. Zone Repeated Measures
+from statsmodels.stats.anova import AnovaRM
+from statsmodels.formula.api import ols
+
+#Remove NaN rows
+s2_grouped = s2_grouped.dropna()
+
+#Repeated Measures ANOVA
+snowANOVA_s2 = ols('depths ~ C(zones, Sum)*C(time, Sum)', data = s2_grouped).fit()
+table = sm.stats.anova_lm(snowANOVA_s2, typ=1, test = 'Chisq')
+print(table)
+
 
 # %%
